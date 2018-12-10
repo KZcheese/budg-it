26cf2313b56e581f13173a593b238a5681a4d620
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-12-10 04:14:33 -0500

    build

---
+++ docs/precache-manifest.fd09857862525fb84aaadc4c67411430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "036f37050b5ea48b02f3",
+    "url": "/static/css/main.72f9395a.chunk.css"
+  },
+  {
+    "revision": "036f37050b5ea48b02f3",
+    "url": "/static/js/main.036f3705.chunk.js"
+  },
+  {
+    "revision": "39903747acac5134ef8e",
+    "url": "/static/js/1.39903747.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11db1ebbc70f8820c8118bb42db664db",
+    "url": "/index.html"
+  }
+];
